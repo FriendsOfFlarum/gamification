12f03ae9320ce8a2c7a9133f9d58d17e1974875d
David Sevilla Martin <me@datitisev.me>
2020-08-28 10:17:26 -0400

    Add migration to update tag permissions from old permission name

---
+++ migrations/2020_08_25_000000_update_tag_permission_names.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of fof/gamification.
+ *
+ * Copyright (c) 2020 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Flarum\Group\Permission;
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Str;
+
+return [
+    'up' => function (Builder $schema) {
+        Permission::query()
+            ->where('permission', 'like', '%.discussion.vote')
+            ->orderBy('permission')
+            ->each(function (Permission $item) use ($schema) {
+                $schema->getConnection()->transaction(function () use ($item) {
+                    $item->delete();
+
+                    $item->permission = $item->permission . 'Posts';
+
+                    if (!Permission::query()->where($item->getAttributes())->exists()) {
+                        $item->save();
+                    }
+                });
+            });
+    },
+    'down' => function (Builder $schema) {
+        Permission::query()
+            ->where('permission', 'like', '%.discussion.votePosts')
+            ->orderBy('permission')
+            ->each(function (Permission $item) use ($schema) {
+                $schema->getConnection()->transaction(function () use ($item) {
+                    $item->delete();
+
+                    $item->permission = Str::replaceLast('votePosts', 'vote', $item->permission);
+
+                    if (!Permission::query()->where($item->getAttributes())->exists()) {
+                        $item->save();
+                    }
+                });
+            });
+    },
+];
