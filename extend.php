49493397c5f019504b940f072cab79aa04224731
datitisev <datitisev@users.noreply.github.com>
2021-04-04 22:00:45 +0000

    Apply fixes from StyleCI

--- extend.php
+++ extend.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of fof/gamification.
  *
- * Copyright (c) 2020 FriendsOfFlarum.
+ * Copyright (c) FriendsOfFlarum.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -163,5 +163,5 @@ return [
         ->listen(Deleted::class, Listeners\RemoveVoteHandler::class),
 
     (new Extend\SimpleFlarumSearch(DiscussionSearcher::class))
-        ->addGambit(HotGambit::class)
+        ->addGambit(HotGambit::class),
 ];
--- src/Commands/DeleteRankHandler.php
+++ src/Commands/DeleteRankHandler.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of fof/gamification.
  *
- * Copyright (c) 2020 FriendsOfFlarum.
+ * Copyright (c) FriendsOfFlarum.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
