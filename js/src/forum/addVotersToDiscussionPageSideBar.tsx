1e99fe83996fbff18bc04ff3cfa676dd5246cab2
David Wheatley <hi@davwheat.dev>
2022-11-06 18:37:22 +0000

    fix: various fixes for Voters component (#97)
    * fix(voters): style improvements on mobile
    
    * fix: add vertical gap between items on mobile
    
    * fix: respect seeVoters permission
    
    Closes #85
    
    * fix: duplicate voters

--- js/src/forum/addVotersToDiscussionPageSideBar.tsx
+++ js/src/forum/addVotersToDiscussionPageSideBar.tsx
@@ -4,16 +4,18 @@ import DiscussionPage from 'flarum/forum/components/DiscussionPage';
 import Voters from './components/Voters';
 import type ItemList from 'flarum/common/utils/ItemList';
 
+import type Mithril from 'mithril';
+
 /**
  * Adds our custom {@link Voters} component to the discussion sidebar.
  */
 export default function addVotersToDiscussionPageSideBar() {
-  extend(DiscussionPage.prototype, 'sidebarItems', function (this: DiscussionPage, items: ItemList) {
+  extend(DiscussionPage.prototype, 'sidebarItems', function (this: DiscussionPage, items: ItemList<Mithril.Chilren>) {
     const discussion = this.discussion;
-    const posts = discussion.posts();
-    const firstPost = posts[0];
+    const posts = discussion!.posts() || [];
+    const firstPost = posts?.[0];
 
-    if (firstPost?.canSeeVotes?.() && !!app.forum.attribute('fof-gamification-op-votes-only')) {
+    if (firstPost?.canSeeVotes?.() && firstPost?.seeVoters?.() && !!app.forum.attribute('fof-gamification-op-votes-only')) {
       items.add('op-voters', <Voters post={firstPost} />, 90);
     }
   });
