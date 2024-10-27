c76fb715e39a8eae7bfa295602766ab45c0dbd47
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-10-27 10:44:08 +0100

    chore: export `RankingImage`

--- js/src/forum/components/index.ts
+++ js/src/forum/components/index.ts
@@ -2,8 +2,10 @@ import RankingsPage from './RankingsPage';
 import VoteNotification from './VoteNotification';
 import VotesModal from './VotesModal';
 import Voters from './Voters';
+import RankingImage from './RankingImage';
 
 export const components = {
+  RankingImage,
   RankingsPage,
   VoteNotification,
   VotesModal,
