5514562499c5cda2de2f243cedf29852fd367255
IanM <16573496+imorland@users.noreply.github.com>
2026-08-03 09:30:29 +0100

    chore: ts convert SettingsPage (#166)

--- js/src/admin/components/UploadImageButton.js
+++
@@ -1,8 +0,0 @@
-import app from 'flarum/admin/app';
-import FlarumUploadImageButton from 'flarum/common/components/UploadImageButton';
-
-export default class UploadImageButton extends FlarumUploadImageButton {
-  resourceUrl() {
-    return app.forum.attribute('apiUrl') + '/' + this.attrs.path;
-  }
-}
